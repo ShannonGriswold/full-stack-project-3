0da684152455beb4c076a7bbe0bed7baca09ee70
ShannonGriswold <shannon.v.griswold@gmail.com>
2024-11-28 15:59:15 -0500

    Switched back to full url instead of proxy

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,15 +2,15 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
-const path = require('path');
+// const path = require('path');
 
-module.exports = {
-  outputDir: path.resolve(__dirname, "../server/public"),
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000'
-      }
-    }
-  }
-}
+// module.exports = {
+//   outputDir: path.resolve(__dirname, "../server/public"),
+//   devServer: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:5000'
+//       }
+//     }
+//   }
+// }

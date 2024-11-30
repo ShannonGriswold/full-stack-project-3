a24284999adc3b285f2bfd84a9be56a35c4098f8
ShannonGriswold <145224530+ShannonGriswold@users.noreply.github.com>
2024-11-29 22:45:10 -0500

    Revert "Add multiple pages"

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -3,63 +3,52 @@ const mongodb = require('mongodb');
 
 const router = express.Router();
 
-//Get all Posts
+//Get Posts
 router.get('/', async (req, res) => {
-    const books = await loadBooksCollection();
-    res.send(await books.find({}).toArray());
-});
-
-//Get one post by id
-router.get('/:id', async (req, res) => {
-    const books = await loadBooksCollection();
-    res.send(await books.findOne({_id: new mongodb.ObjectId(req.params.id)}));
+    const posts = await loadPostsCollection();
+    res.send(await posts.find({}).toArray());
 });
 
 //Add Posts
 router.post('/', async (req, res) => {
-    const books = await loadBooksCollection();
-    await books.insertOne({
+    const posts = await loadPostsCollection();
+    await posts.insertOne({
         title: req.body.title,
-        author: req.body.author,
-        status: req.body.status,
-        series: req.body.series,
-        genre: req.body.genre,
-        format: req.body.format,
-        notes: req.body.notes,
+        description: req.body.description,
+        deadline: req.body.deadline,
+        priority: req.body.priority,
+        isComplete: req.body.isComplete
     });
     res.status(201).send();
 });
 
 //Update Posts
 router.post('/:id', async (req, res) => {
-    const books = await loadBooksCollection();
+    const posts = await loadPostsCollection();
     const updatedTask = {
         $set: {
-            title: req.body.title,
-            author: req.body.author,
-            status: req.body.status,
-            series: req.body.series,
-            genre: req.body.genre,
-            format: req.body.format,
-            notes: req.body.notes,
+            description: req.body.description,
+            deadline: req.body.deadline,
+            priority: req.body.priority,
+            isComplete: req.body.isComplete
         },
     }
-    await books.updateOne({_id: new mongodb.ObjectId(req.params.id)}, updatedTask);
+    await posts.updateOne({_id: new mongodb.ObjectId(req.params.id)}, updatedTask);
     res.status(200).send();
 });
 
 //Delete Posts
 router.delete('/:id', async (req, res) => {
-    const books = await loadBooksCollection();
-    await books.deleteOne({_id: new mongodb.ObjectId(req.params.id)});
+    const posts = await loadPostsCollection();
+    await posts.deleteOne({_id: new mongodb.ObjectId(req.params.id)});
     res.status(200).send();
 });
 
 
-async function loadBooksCollection() {
+async function loadPostsCollection() {
     const client = await mongodb.MongoClient.connect('mongodb+srv://shannonGriswold:FullStack@fullstackproject3.nyzxi.mongodb.net/?retryWrites=true&w=majority&appName=FullStackProject3');
 
-    return client.db('FullStack').collection('books');
+    return client.db('FullStack').collection('tasks');
 }
 
 module.exports = router;
